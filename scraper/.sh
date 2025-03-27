5714b82c6dc6ed95439f173e9aec8d0b17b4b99e
EmiBrmt <emilie.brimont@edu.devinci.fr>
2025-03-27 14:15:49 +0100

    Modifications du scraper et mise à jour automatique CSV

---
+++ scraper/.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+URL="https://fr.advfn.com/bourses/SPI/SP500/cours"
+
+curl -s "$URL" -o sp500_cours.html
+
+price=$(sed -n '/<div>Dernier<\/div>/,/<\/div>/p' sp500_cours.html | grep -oP '\d{1,3}(?: \d{3})*(?:,\d{2})?')
+
+echo "$(date '+%Y-%m-%d %H:%M:%S'), $price" >> sp500_prices.csv
--- scraper/scraper.sh
+++ scraper/scraper.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-# URL à scraper
 URL="https://fr.advfn.com/bourses/SPI/SP500/cours"
 
-# Boucle infinie
 while true
 do
-  # Télécharger la page HTML
   curl -s "$URL" -o sp500_cours.html
-
-  # Extraire le cours (ex: 5 667,56)
   price=$(sed -n '/<div>Dernier<\/div>/,/<\/div>/p' sp500_cours.html | grep -oP '\d{1,3}(?: \d{3})*(?:,\d{2})?')
-
-  # Sauvegarder avec timestamp
   echo "$(date '+%Y-%m-%d %H:%M:%S'), $price" >> sp500_prices.csv
 
-  # Attendre 5 min
+  # Commit & push automatique
+  git add sp500_prices.csv
+  git commit -m "MAJ auto - $(date '+%Y-%m-%d %H:%M:%S')" > /dev/null 2>&1
+  git push origin main > /dev/null 2>&1
+
   sleep 300
 done
