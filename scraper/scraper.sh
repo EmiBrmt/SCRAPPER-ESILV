f5c843b979b1f5ec41ec50f0daff3903e0d1ed64
EmiBrmt <emilie.brimont@edu.devinci.fr>
2025-03-27 14:31:23 +0100

    Ajout du dashboard Dash et correction du scraper

--- scraper/scraper.sh
+++ scraper/scraper.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-
 URL="https://fr.advfn.com/bourses/SPI/SP500/cours"
-
 while true
 do
-  curl -s "$URL" -o sp500_cours.html
-  price=$(sed -n '/<div>Dernier<\/div>/,/<\/div>/p' sp500_cours.html | grep -oP '\d{1,3}(?: \d{3})*(?:,\d{2})?')
-  echo "$(date '+%Y-%m-%d %H:%M:%S'), $price" >> sp500_prices.csv
-
-  # Commit & push automatique
-  git add sp500_prices.csv
-  git commit -m "MAJ auto - $(date '+%Y-%m-%d %H:%M:%S')" > /dev/null 2>&1
-  git push origin main > /dev/null 2>&1
-
-  sleep 300
+curl -s "$URL" -o sp500_cours.html
+price=$(sed -n '/<div>Dernier<\/div>/,/<\/div>/p' sp500_cours.html | grep -oP '\d{1,3}(?: \d{3})*(?:,\d{2})?' | tail -1)
+echo "$(date '+%Y-%m-%d %H:%M:%S'), $price" >> sp500_prices.csv
+# Commit & push automatique
+git add sp500_prices.csv
+git commit -m "MAJ auto - $(date '+%Y-%m-%d %H:%M:%S')" > /dev/null 2>&1
+git push origin main > /dev/null 2>&1
+sleep 300
 done
